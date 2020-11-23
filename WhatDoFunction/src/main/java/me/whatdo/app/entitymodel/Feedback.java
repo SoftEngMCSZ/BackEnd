53ca5faef5ad15fdfb0f21c9e2aba750022f596e
mmang101 <mxmangini@wpi.edu>
2020-11-23 13:47:42 -0500

    - Added serialization to Feedback

--- WhatDoFunction/src/main/java/me/whatdo/app/entitymodel/Feedback.java
+++ WhatDoFunction/src/main/java/me/whatdo/app/entitymodel/Feedback.java
@@ -1,16 +1,28 @@
 package me.whatdo.app.entitymodel;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.Date;
 
 public class Feedback {
 	private final Collaborator author;
 	private final Date timestamp;
 	private final String content;
+	private static final  Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
 	public Feedback(Collaborator author, Date timestamp, String content) {
 		this.author = author;
 		this.timestamp = timestamp;
 		this.content = content;
+	}
+
+	public String toJson(){
+		return gson.toJson(this);
+	}
+
+	public static Feedback fromJson(String json){
+		return gson.fromJson(json, Feedback.class);
 	}
 
 	public Collaborator getAuthor() {

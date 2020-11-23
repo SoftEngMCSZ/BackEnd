6c4b4005957f869e3418575534dcc02ded87de98
mmang101 <mxmangini@wpi.edu>
2020-11-23 17:05:57 -0500

    - made stubs to two new user handlers

---
+++ WhatDoFunction/src/main/java/me/whatdo/app/handlers/user/AddUserHandler.java
@@ -0,0 +1,31 @@
+package me.whatdo.app.handlers.user;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class AddUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        Map<String, String> pathParam = input.getPathParameters();
+        UUID choiceID = UUID.fromString(pathParam.get("choiceID"));
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        return response
+                .withBody("{}")
+                .withStatusCode(404);
+    }
+}
+

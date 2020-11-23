9cfc902144f4f3932f12bef5fb457bddc91e14c9
mmang101 <mxmangini@wpi.edu>
2020-11-23 13:43:10 -0500

    - Added serialization to ChoiceRequest

--- WhatDoFunction/src/main/java/me/whatdo/app/entitymodel/ChoiceRequest.java
+++ WhatDoFunction/src/main/java/me/whatdo/app/entitymodel/ChoiceRequest.java
@@ -1,15 +1,27 @@
 package me.whatdo.app.entitymodel;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.List;
 
 public class ChoiceRequest {
 	protected final String content;
 	protected final List<Alternative> alternatives;
 	protected final int maxCollaborators;
+	protected static final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
 	public ChoiceRequest(String content, List<Alternative> alts, int maxCollaborators) {
 		this.content = content;
 		this.alternatives = alts;
 		this.maxCollaborators = maxCollaborators;
+	}
+
+	public String toJson(){
+		return gson.toJson(this);
+	}
+
+	public static ChoiceRequest fromJson(String json){
+		return gson.fromJson(json, ChoiceRequest.class);
 	}
 }

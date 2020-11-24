d24b17b724ca82ced4a6a8547ef5e5a476732b67
mmang101 <mxmangini@wpi.edu>
2020-11-23 21:43:17 -0500

    - refactored Choice lambdas for readability

--- WhatDoFunction/src/main/java/me/whatdo/app/handlers/choice/CreateChoiceHandler.java
+++ WhatDoFunction/src/main/java/me/whatdo/app/handlers/choice/CreateChoiceHandler.java
@@ -8,6 +8,7 @@ import com.google.gson.JsonObject;
 import me.whatdo.app.db.ChoiceDAO;
 import me.whatdo.app.entitymodel.Choice;
 import me.whatdo.app.entitymodel.ChoiceRequest;
+import me.whatdo.app.handlers.auth.UserAuthHandler;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -17,40 +18,43 @@ import java.util.UUID;
 public class CreateChoiceHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+
+        //Response related Instantiation
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
-
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-
         JsonObject body = new JsonObject();
+
+        // Business Logic Instantiation
+        ChoiceRequest request;
+        Choice choice;
+        ChoiceDAO dao;
+
         try {
-            if (input.getHttpMethod().equals("POST")) {
-                ChoiceRequest request = ChoiceRequest.fromJson(input.getBody());
-                if (validateRequest(request)) {
-                    Choice choice = new Choice(request);
-                    ChoiceDAO dao = new ChoiceDAO();
-                    try {
-                        if (dao.addChoice(choice)) {
-                            return response.withBody(choice.toJson()).withHeaders(headers).withStatusCode(201);
-                        } else {
-                            body.addProperty("Message", "400 unable to insert Choice to DB");
-                            return response.withBody(body.toString()).withHeaders(headers).withStatusCode(400);
-                        }
-                    } catch (Exception e) {
-                        body.addProperty("Message", "500 server error");
-                        body.addProperty("Error", e.getMessage());
-                        return response.withBody(body.toString()).withHeaders(headers).withStatusCode(500);
-                    }
-                } else {
-                    body.addProperty("Message", "400 malformed ChoiceRequest");
-                    return response.withBody(body.toString()).withHeaders(headers).withStatusCode(400);
-                }
-            } else {
+            // First Check: HTTP Method
+            if (!input.getHttpMethod().equals("POST")) {
                 body.addProperty("Message", "405 method not allowed");
                 return response.withBody(body.toString()).withHeaders(headers).withStatusCode(405);
             }
+            // Second Check: Validate request schema
+            request = ChoiceRequest.fromJson(input.getBody());
+            if (!validateRequest(request)) {
+                body.addProperty("Message", "400 malformed ChoiceRequest");
+                return response.withBody(body.toString()).withHeaders(headers).withStatusCode(400);
+            }
+            choice = new Choice(request);
+            dao = new ChoiceDAO();
+            // Third Check: presence of Choice in database already
+            if (!dao.addChoice(choice)) {
+                body.addProperty("Message", "400 unable to insert Choice to DB");
+                return response.withBody(body.toString()).withHeaders(headers).withStatusCode(400);
+            }
+            // Successfully returned Choice
+            return response.withBody(choice.toJson()).withHeaders(headers).withStatusCode(201);
+
+            // Catch unexpected server errors
         } catch (Exception e){
             body.addProperty("Message", "500 server error");
             body.addProperty("Error", e.getMessage());
@@ -58,9 +62,8 @@ public class CreateChoiceHandler implements RequestHandler<APIGatewayProxyReques
                     .withBody(body.toString())
                     .withStatusCode(500);
         }
-
     }
-
+    // Used to validate all required fields present
     private static boolean validateRequest(ChoiceRequest req){
         JsonObject object = req.toJsonObject();
         return object.has("question") &&
--- WhatDoFunction/src/main/java/me/whatdo/app/handlers/choice/ViewChoiceHandler.java
+++ WhatDoFunction/src/main/java/me/whatdo/app/handlers/choice/ViewChoiceHandler.java
@@ -8,6 +8,7 @@ import com.google.gson.JsonObject;
 import me.whatdo.app.db.ChoiceDAO;
 import me.whatdo.app.entitymodel.Choice;
 import me.whatdo.app.entitymodel.ChoiceRequest;
+import me.whatdo.app.handlers.auth.UserAuthHandler;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -18,49 +19,59 @@ import java.util.UUID;
 public class ViewChoiceHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
+
+        // Response related instantiation
+        Map<String,String>  headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
-
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-
         JsonObject body = new JsonObject();
+
+        // Business Logic Instantiation
+        UUID choiceID;
+        ChoiceDAO dao = new ChoiceDAO();
+
+        // Catch any unexpected errors
         try {
-            if (input.getHttpMethod().equals("GET")) {
-                UUID choiceID;
-                Map<String, String> pathParams = input.getPathParameters();
-                if (pathParams.containsKey("choiceID")) {
-                    try {
-                        choiceID = UUID.fromString(pathParams.get("choiceID"));
-                    } catch (IllegalArgumentException e) {
-                        body.addProperty("Message","400 malformed choiceID");
-                        body.addProperty("Error", e.getMessage());
-                        return response.withBody(body.toString()).withHeaders(headers).withStatusCode(400);
-                    }
-                    ChoiceDAO dao = new ChoiceDAO();
-                    Optional<Choice> choice = dao.getChoice(choiceID);
-                    if(choice.isPresent()){
-                        return response.withBody(choice.get().toJson()).withHeaders(headers).withStatusCode(200);
-                    } else {
-                        body.addProperty("Message","404 Choice not found");
-                        body.addProperty("Received", choiceID.toString());
-                        return response.withBody(body.toString()).withHeaders(headers).withStatusCode(404);
-                    }
-                } else {
-                    body.addProperty("Message", "400 missing choiceID paramater");
-                    return response
-                            .withBody(body.toString())
-                            .withHeaders(headers)
-                            .withStatusCode(400);
-                }
-            } else {
+            // First Check: HTTP Method
+            if (!input.getHttpMethod().equals("GET")) {
                 body.addProperty("Message", "405 method not allowed");
                 return response
                         .withBody(body.toString())
                         .withHeaders(headers)
                         .withStatusCode(405);
             }
+            // Second Check: choiceID presence in path
+            Map<String, String> pathParams = input.getPathParameters();
+            if (!pathParams.containsKey("choiceID")) {
+                body.addProperty("Message", "400 missing choiceID paramater");
+                return response
+                        .withBody(body.toString())
+                        .withHeaders(headers)
+                        .withStatusCode(400);
+            }
+            // Third Check: Malformed ID
+            try {
+                choiceID = UUID.fromString(pathParams.get("choiceID"));
+            } catch (IllegalArgumentException e) {
+                body.addProperty("Message", "400 malformed choiceID");
+                body.addProperty("Error", e.getMessage());
+                return response.withBody(body.toString()).withHeaders(headers).withStatusCode(400);
+            }
+
+            // Fourth Check: Choice presence in Database
+            Optional<Choice> choice = dao.getChoice(choiceID);
+            if (!choice.isPresent()) {
+                body.addProperty("Message", "404 Choice not found");
+                body.addProperty("Received", choiceID.toString());
+                return response.withBody(body.toString()).withHeaders(headers).withStatusCode(404);
+            }
+
+            // Successfully handled and returned
+            return response.withBody(choice.get().toJson()).withHeaders(headers).withStatusCode(200);
+
+            //Some other 500 server error arose
         } catch (Exception e) {
             body.addProperty("Message", "500 server error");
             body.addProperty("Error", e.getMessage());

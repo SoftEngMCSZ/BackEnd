749ff938ce7d9cb2291d6b9b91ce2b03cf24a58b
mmang101 <mxmangini@wpi.edu>
2020-11-24 12:26:01 -0500

    - Add seperation of rol for authenticaton and De/endcoding

--- WhatDoFunction/src/main/java/me/whatdo/app/handlers/auth/UserAuthHandler.java
+++ WhatDoFunction/src/main/java/me/whatdo/app/handlers/auth/UserAuthHandler.java
@@ -9,19 +9,33 @@ import java.util.UUID;
 
 public class UserAuthHandler {
 	public static boolean isUserAuthenticated(String authHeader, UUID choiceId) throws Exception {
-		Base64.Decoder decoder = Base64.getDecoder();
+		System.out.println("Received:"+authHeader);
+
 		CollaboratorDAO dao = new CollaboratorDAO();
 
-		String[] usernameAndPassword = new String(decoder.decode(authHeader.getBytes())).split(":");
+		String[] usernameAndPassword = decode(authHeader).split(":");
 		String username = usernameAndPassword[0];
 		String password = usernameAndPassword[1];
+
+		System.out.println(username+" : "+password);
 
 		Optional<Collaborator> collab = dao.getCollaborator(choiceId,username);
 
 		if(collab.isPresent()) {
+			System.out.println("Present");
 			boolean out = collab.get().verifyPassword(password);
 			return out;
 		}
 		return false;
+	}
+
+	private static String decode(String encodedStr){
+		Base64.Decoder decoder = Base64.getDecoder();
+		return new String(decoder.decode(encodedStr));
+	}
+
+	public static String encode(String plainStr){
+		Base64.Encoder encoder = Base64.getEncoder();
+		return new String(encoder.encode(plainStr.getBytes()));
 	}
 }

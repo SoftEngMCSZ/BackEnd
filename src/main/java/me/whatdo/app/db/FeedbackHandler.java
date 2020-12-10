3b0ba6814fd050f6b8e1a07a771b079ecc44b1b8
mmang101 <mxmangini@wpi.edu>
2020-12-10 15:34:17 -0500

    - feedback handler complete

---
+++ src/main/java/me/whatdo/app/db/FeedbackHandler.java
@@ -0,0 +1,73 @@
+package me.whatdo.app.db;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.JsonObject;
+import me.whatdo.app.model.ApiResponse;
+import me.whatdo.app.model.entity.Alternative;
+import me.whatdo.app.model.entity.Choice;
+import me.whatdo.app.model.entity.Feedback;
+import me.whatdo.app.model.request.FeedbackRequest;
+import me.whatdo.app.model.request.FinalRequest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class FeedbackHandler implements RequestHandler<FeedbackRequest, ApiResponse> {
+
+		public ApiResponse handleRequest(final FeedbackRequest input, final Context context) {
+			JsonObject body = new JsonObject();
+			ChoiceDAO choiceDAO = new ChoiceDAO();
+			AlternativeDAO altDAO = new AlternativeDAO();
+			try {
+				if (!validateRequest(input.toJsonObject())) {
+					body.addProperty("Message", "400 malformed FinalRequest");
+					body.addProperty("Input", input.toJson());
+					return new ApiResponse(400, body.toString());
+				}
+
+				UUID choiceId, altId;
+				try {
+					choiceId = UUID.fromString(input.getChoiceId());
+					altId = UUID.fromString(input.getAlternativeId());
+				} catch (IllegalArgumentException e) {
+					body.addProperty("Message", "400 ID malformed");
+					return new ApiResponse(400, body.toString());
+				}
+
+				Optional<Choice> choiceOpt = choiceDAO.getChoice(choiceId);
+
+				if(!choiceOpt.isPresent()){
+					body.addProperty("Message", "404 Choice not found");
+					return new ApiResponse(404, body.toString());
+				}
+
+				Optional<Alternative> altOpt = altDAO.getAlternative(altId);
+
+				if (!altOpt.isPresent()) {
+					body.addProperty("Message", "404 Alternative not found");
+					return new ApiResponse(404, body.toString());
+				}
+
+				if (!altOpt.get().addFeedback(new Feedback(input))) {
+					body.addProperty("Message", "400 could not add feedback to Alternative");
+					return new ApiResponse(400, body.toString());
+				}
+				choiceOpt = choiceDAO.getChoice(choiceId);
+				assert choiceOpt.isPresent();
+
+				return new ApiResponse(200, choiceOpt.get().toJson());
+
+			} catch (Exception e) {
+				body.addProperty("Message", "500 server error");
+				body.addProperty("Error", e.getMessage());
+				return new ApiResponse(500, body.toString());
+			}
+		}
+
+		private static boolean validateRequest(JsonObject req) {
+			return req.has("alternativeId") &&
+					req.has("contents") &&
+					req.has("choiceId");
+		}
+}
